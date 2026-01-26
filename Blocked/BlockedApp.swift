eb6ff080f6726807283ae57a6fa1ae0b3fe43cf5
alexb <alexbhandari37@gmail.com>
2026-01-25 22:03:00 -0800

    Initial Commit

---
+++ Blocked/BlockedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockedApp.swift
+//  Blocked
+//
+//  Created by Alex on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
