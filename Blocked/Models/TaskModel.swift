8964c6078ac44c0124a061cf048cd60d237ab100
alexb <alexbhandari37@gmail.com>
2026-01-29 19:41:09 -0800

    Create a draft Task object and View to insert schedule json

---
+++ Blocked/Models/TaskModel.swift
@@ -0,0 +1,17 @@
+//
+//  TaskModel.swift
+//  Blocked
+//
+//  Created by Alex on 1/29/26.
+//
+
+import Foundation
+
+
+struct Task{
+    
+    let TaskName: String
+    let BlockType: String
+    let TimesPerWeek: Int
+    
+}
---
+++ Blocked/Views/ScheduleCreatorView.swift
@@ -0,0 +1,37 @@
+//
+//  ScheduleCreatorView.swift
+//  Blocked
+//
+//  Created by Alex on 1/29/26.
+//
+
+import SwiftUI
+
+struct ScheduleCreatorView: View {
+    
+    @State var jsonSchedule: String = ""
+    
+    var body: some View {
+        
+        Text("Paste Your JSON Format Schedule Here")
+        TextEditor(text: $jsonSchedule)
+            .frame(minHeight: 500, maxHeight: 500)   // <- set your fixed height
+            .scrollContentBackground(.hidden)        // iOS 16+ (optional, nicer)
+            .padding(8)
+            .background(.thinMaterial)               // or Color(.secondarySystemBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.quaternary)
+            )
+        Button("Create Schedule") {
+            
+        }
+        
+        Spacer()
+    }
+}
+
+#Preview {
+    ScheduleCreatorView()
+}
